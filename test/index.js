5378cf2eca1232fee8f90bbfd503123edd2b65a1
Oron Nadiv <oron.nadiv@lanetix.com>
2016-03-17 11:57:22 -0700

    A few changes: 1. Move form eslint to standardjs. 2. upgrade 'should' to the newest version. 3. Bug fix - clean a few references when calling Close().

--- test/index.js
+++ test/index.js
@@ -1,10 +1,10 @@
 /*globals it:false*/
-'use strict';
+'use strict'
 
-var amqpUrl = 'amqp://guest:guest@localhost:5672',
-  childProcess = require('child_process'),
-  BPromise = require('bluebird'),
-  amqp = require('../index')(amqpUrl);
+var amqpUrl = 'amqp://guest:guest@localhost:5672'
+var childProcess = require('child_process')
+var BPromise = require('bluebird')
+var amqp = require('../index')(amqpUrl)
 
 describe('amqplib-easy', function () {
   afterEach(function () {
@@ -12,160 +12,164 @@ describe('amqplib-easy', function () {
       .then(function (connection) {
         return connection.createChannel()
           .then(function (channel) {
-            return BPromise.all([
-              channel.checkQueue('found_cats')
-                .then(function () {
-                  return channel.deleteQueue('found_cats');
-                }),
-              channel.checkQueue('found_cats.failure')
-                .then(function () {
-                  return channel.deleteQueue('found_cats.failure');
-                })
-            ])
+            return BPromise.all(
+              [
+                channel.checkQueue('found_cats')
+                  .then(function () {
+                    return channel.deleteQueue('found_cats')
+                  }),
+                channel.checkQueue('found_cats.failure')
+                  .then(function () {
+                    return channel.deleteQueue('found_cats.failure')
+                  })
+              ])
               .catch(function () {
-                //the queue doesn't exist, so w/e
-                return;
-              });
-          });
-      });
-  });
+                // the queue doesn't exist, so w/e
+                return
+              })
+          })
+      })
+  })
 
   describe('consumer', function () {
-    var cancel;
+    var cancel
 
     afterEach(function () {
       if (cancel) {
-        return cancel();
+        return cancel()
       }
-    });
+    })
 
     it('should accept alternate parser', function (done) {
       amqp.consume(
         {
           exchange: 'cat',
-          parse: function () { return { name: 'Fred' }; },
+          parse: function () {
+            return {name: 'Fred'}
+          },
           queue: 'found_cats',
-          topics: [ 'found.*' ]
+          topics: ['found.*']
         },
         function (cat) {
-          var name = cat.json.name;
+          var name = cat.json.name
           try {
-            name.should.equal('Fred');
-            done();
-          } catch (err) { done(err); }
+            name.should.equal('Fred')
+            done()
+          } catch (err) {
+            done(err)
+          }
         }
       )
         .then(function (c) {
-          cancel = c;
+          cancel = c
           return BPromise.all([
-            amqp.sendToQueue({ queue: 'found_cats' }, new Buffer('dsadasd'))
-          ]);
+            amqp.sendToQueue({queue: 'found_cats'}, new Buffer('dsadasd'))
+          ])
         })
-        .catch(done);
-    });
+        .catch(done)
+    })
 
     it('should handle buffers reasonably', function (done) {
-      var catCount = 0;
+      var catCount = 0
       amqp.consume(
         {
           exchange: 'cat',
           queue: 'found_cats',
-          topics: [ 'found.*' ]
+          topics: ['found.*']
         },
         function (cat) {
-          var name = cat.json.name;
+          var name = cat.json.name
           try {
-            /*eslint-disable no-unused-expressions*/
-            (name === 'Sally' || name === 'Fred').should.be.ok;
-            /*eslint-enable no-unused-expressions*/
+            (name === 'Sally' || name === 'Fred').should.be.ok()
             if (++catCount === 2) {
-              done();
+              done()
             }
-          } catch (err) { done(err); }
+          } catch (err) {
+            done(err)
+          }
         }
       )
         .then(function (c) {
-          cancel = c;
+          cancel = c
           return BPromise.all([
-            amqp.publish({ exchange: 'cat' }, 'found.tawny', new Buffer('{ "name": "Sally" }')),
-            amqp.sendToQueue({ queue: 'found_cats' }, new Buffer('{ "name": "Fred" }'))
-          ]);
+            amqp.publish({exchange: 'cat'}, 'found.tawny', new Buffer('{ "name": "Sally" }')),
+            amqp.sendToQueue({queue: 'found_cats'}, new Buffer('{ "name": "Fred" }'))
+          ])
         })
-        .catch(done);
-    });
+        .catch(done)
+    })
 
     it('should publish, sendToQueue and receive', function (done) {
-      var catCount = 0;
+      var catCount = 0
       amqp.consume(
         {
           exchange: 'cat',
           queue: 'found_cats',
-          topics: [ 'found.*' ]
+          topics: ['found.*']
         },
         function (cat) {
-          var name = cat.json.name;
+          var name = cat.json.name
           try {
-            /*eslint-disable no-unused-expressions*/
-            (name === 'Sally' || name === 'Fred').should.be.ok;
-            /*eslint-enable no-unused-expressions*/
+            (name === 'Sally' || name === 'Fred').should.be.ok()
             if (++catCount === 2) {
-              done();
+              done()
             }
-          } catch (err) { done(err); }
+          } catch (err) {
+            done(err)
+          }
         }
       )
         .then(function (c) {
-          cancel = c;
+          cancel = c
           return BPromise.all([
-            amqp.publish({ exchange: 'cat' }, 'found.tawny', { name: 'Sally' }),
-            amqp.sendToQueue({ queue: 'found_cats' }, { name: 'Fred' })
-          ]);
+            amqp.publish({exchange: 'cat'}, 'found.tawny', {name: 'Sally'}),
+            amqp.sendToQueue({queue: 'found_cats'}, {name: 'Fred'})
+          ])
         })
-        .catch(done);
-
-    });
+        .catch(done)
+    })
 
     it('should publish even if something causes the channel to die', function (done) {
       amqp.consume(
         {
           exchange: 'cat',
           queue: 'found_cats',
-          topics: [ 'found.*' ]
+          topics: ['found.*']
         },
         function () {
-          done();
+          done()
         }
       )
         .then(function (c) {
-          cancel = c;
-          return amqp.publish({ exchange: 'cat', exchangeType: 'direct' }, 'found.tawny', { name: 'Sally' })
+          cancel = c
+          return amqp.publish({exchange: 'cat', exchangeType: 'direct'}, 'found.tawny', {name: 'Sally'})
             .catch(function () {
-              return amqp.publish({ exchange: 'cat', exchangeType: 'topic' }, 'found.tawny', { name: 'Sally' });
-            });
+              return amqp.publish({exchange: 'cat', exchangeType: 'topic'}, 'found.tawny', {name: 'Sally'})
+            })
         })
-        .catch(done);
-    });
+        .catch(done)
+    })
 
     describe('fanout exchange', function () {
-
-      function deleteCat() {
+      function deleteCat () {
         return amqp.connect()
           .then(function (connection) {
             return connection.createChannel()
               .then(function (channel) {
                 channel.checkExchange('cat')
-                .then(
-                  function () {
-                    channel.deleteExchange('cat');
-                  },
-                  function () { /* NBD it doesn't exist */ }
-                );
-              });
-          });
+                  .then(
+                    function () {
+                      channel.deleteExchange('cat')
+                    },
+                    function () { /* NBD it doesn't exist */
+                    }
+                )
+              })
+          })
       }
 
-      beforeEach(deleteCat);
-      afterEach(deleteCat);
+      beforeEach(deleteCat)
+      afterEach(deleteCat)
 
       it('should publish via fanout', function (done) {
         amqp.consume(
@@ -175,86 +179,84 @@ describe('amqplib-easy', function () {
             queue: 'found_cats'
           },
           function (cat) {
-            var name = cat.json.name;
+            var name = cat.json.name
             try {
-              name.should.equal('Sally');
-              done();
-            } catch (err) { done(err); }
+              name.should.equal('Sally')
+              done()
+            } catch (err) {
+              done(err)
+            }
           }
         )
           .then(function (c) {
-            cancel = c;
+            cancel = c
             return amqp.publish(
-              { exchange: 'cat', exchangeType: 'fanout' },
+              {exchange: 'cat', exchangeType: 'fanout'},
               'found.tawny',
-              { name: 'Sally' }
-            );
+              {name: 'Sally'}
+            )
           })
-          .catch(done);
-      });
-    });
-  });
-
+          .catch(done)
+      })
+    })
+  })
 
   it('should cancel consumer', function (done) {
-
     amqp.consume(
       {
         exchange: 'cat',
         queue: 'found_cats',
-        topics: [ 'found.*' ]
+        topics: ['found.*']
       },
       function () {
-        done('Got a cat');
+        done('Got a cat')
       }
     )
       .then(function (cancel) {
-        return cancel();
+        return cancel()
       })
       .then(function () {
-        //add some delay so that the cancel takes affect
-        return BPromise.delay(200);
+        // add some delay so that the cancel takes affect
+        return BPromise.delay(200)
       })
       .then(function () {
-        return amqp.sendToQueue({ queue: 'found_cats' }, { name: 'Carl' });
+        return amqp.sendToQueue({queue: 'found_cats'}, {name: 'Carl'})
       })
       .then(function () {
-        //add some delay so that we get the sent message if it were sent
-        return BPromise.delay(200);
+        // add some delay so that we get the sent message if it were sent
+        return BPromise.delay(200)
       })
-      .nodeify(done);
-
-  });
-});
+      .nodeify(done)
+  })
+})
 
 describe('Connection managment', function () {
-
   it('should reuse the existing connection', function (done) {
     amqp.connect()
       .then(function (connection1) {
         amqp.connect()
           .then(function (connection2) {
-            connection1.should.equal(connection2);
-            done();
-          });
+            connection1.should.equal(connection2)
+            done()
+          })
       })
-      .catch(done);
-  });
+      .catch(done)
+  })
 
   it('should close the connection upon death', function (done) {
-    this.timeout(3000);
+    this.timeout(3000)
     // Spin up a process to kill
-    var testProcess = childProcess.fork('./test/resources/death.js', { silent: false });
+    var testProcess = childProcess.fork('./test/resources/death.js', {silent: false})
 
     testProcess.on('message', function (message) {
       switch (message) {
         case 'ok':
-          return done();
+          return done()
         case 'ready':
-          return testProcess.kill('SIGTERM');
+          return testProcess.kill('SIGTERM')
         default:
-          return done(new Error('Unknown message ' + message));
+          return done(new Error('Unknown message ' + message))
       }
-    });
-  });
-});
+    })
+  })
+})
--- test/resources/death.js
+++ test/resources/death.js
@@ -1,16 +1,16 @@
-'use strict';
+'use strict'
 
-var amqp = require('../../index')('amqp://guest:guest@localhost:5672'),
-  diehard = require('diehard');
+var amqp = require('../../index')('amqp://guest:guest@localhost:5672')
+var diehard = require('diehard')
 
 amqp.connect()
   .then(function (connection) {
     connection.on('close', function () {
-      process.send('ok');
-    });
+      process.send('ok')
+    })
   })
   .then(function () {
-    process.send('ready');
-  });
+    process.send('ready')
+  })
 
-diehard.listen();
+diehard.listen()
